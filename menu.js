612d2c334e9aa198a29efc47f0055dbdb6dbbd26
sndiinrmn <Sandinourman@gmail.com>
2025-01-16 21:13:25 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,18 +1,19 @@
+
 let menuItems = [
-{Text: 'home', url: '/'},
-  {Text: 'about', url: '/about.html'},
-  {Text: 'teams', url: '/teaams.html'}
-  ,{Text: 'blog', url: 'blog.html'},
-  {Text: 'course', url: '/course.html'},
-  {Text: 'portofolio', url: '/portofolio.html'},
+    {Text: 'Home', url: '/'},
+    {Text: 'About', url: '/about.html'},
+    {Text: 'Teams', url: '/teams.html'},
+    {Text: 'Blog', url: '/blog.html'},
+    {Text: 'Course', url: '/course.html'},
+    {Text: 'Portfolio', url: '/portfolio.html'},
 ]
-let menu = document.createElement('ul')
-menuItems. forEach(item => {
-    let menuItems = document.createElement('li')
-    let link = document.createElement('a')
-    link.href = item.url
-    link.textContent = item.Text
-    menuItems.appendChild(link)
-    menu.appendChild(menuItems)
+let menu = document.createElement('ul');
+menuItems.forEach(item => {
+    let menuItems = document.createElement('li');
+    let link = document.createElement('a');
+    link.href = item.url;
+    link.innerText = item.Text;
+    menuItems.appendChild(link);
+    menu.appendChild(menuItems);
 })
-document.body.appendChild(menu)
+document.body.appendChild(menu);
